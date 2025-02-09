5098f8b3f74e356fac6c80e19fd8a1b1d0a8451b
Isai-pexel <ibadillam@vlacademy.com>
2025-02-08 18:50:24 -0600

    Funcion realizarSuma

--- JS/PagesJS/Practica_Calculadora.js
+++ JS/PagesJS/Practica_Calculadora.js
@@ -1,9 +1,22 @@
 
 console.log(`Funcionando Archivo: Practica_Calculadora`);
 
- function enviarDatos() {
+function enviarDatos() {
     let nombre = document.getElementById("nombre").value;
     let edad = document.getElementById("edad").value;
 
+    //Mostrar
+    //Alert, console.log, inner
+    //cl
+    console.log('Mostrar Nombre:', nombre);
+    console.log('Mostrar Edad:', edad);
+    alert(`Nombre : ${nombre} Edad: ${edad}`);
 }
 
+function realizarSuma() {
+    console.log("Funcionando realizarSuma")
+    let numero1 = document.getElementById('numero1').value;
+    let numero2 = document.getElementById('numero2').value;
+    console.log(numero1)
+    console.log(numero2)
+}
