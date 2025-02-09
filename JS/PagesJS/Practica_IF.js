0cf6b4f13961b6cd92b82a1a69d3b3a80c8ad352
Isai-pexel <ibadillam@vlacademy.com>
2025-02-08 23:28:16 -0600

    Entregable 1 Proyecto

--- JS/PagesJS/Practica_IF.js
+++ JS/PagesJS/Practica_IF.js
@@ -2,7 +2,7 @@ console.log(`Funcionando Archivo: Practica_IF`);
 
 //paso 1
 
-function verificarEdad(){
+function verificarEdad() {
     let edad = document.getElementById('edad').value;
     let pais = document.getElementById('pais').value;
 
@@ -12,24 +12,69 @@ function verificarEdad(){
     //IfElse
     if (edad >= 18) {
         alert(`No es mayor de edad en ${pais}`);
-        
+
     } else {
         alert(`Es mayor de edad en ${pais}`);
-        
+
     }
 
 }
 
-function verificarParImpar(){
+function verificarParImpar() {
     let numero3 = document.getElementById('numero3').value;
 
     let numeroConvertido = parseInt(numero3);
     if (numeroConvertido % 2 === 0) {
-        alert(`El número ${numero3} es par`);
-        
+        document.getElementById('mensajeParImpar').innerText = `El número ${numero3} es par`;
+
     } else {
-        alert(`El número ${numero3} es impar`);
-        
+        document.getElementById('mensajeParImpar').innerText = `El número ${numero3} es impar`;
+
     }
-    console.log(numero3)
+    console.log(numeroConvertido)
+}
+
+function evaluarNota() {
+    // Mayor a 90 excelente nota
+    // Mayor a 70 aprobado
+    // Mayor a 70 reprobado
+    let nota = document.getElementById('nota').value;
+    let numeroConvertido = parseInt(nota);
+
+    if (numeroConvertido >= 90) {
+        document.getElementById('mensajeNota').innerText = `${nota} Excelente nota`;
+    } else if (numeroConvertido >= 70) {
+        document.getElementById('mensajeNota').innerText = `Aprobado con ${nota} `;
+    } else {
+        document.getElementById('mensajeNota').innerText = `Reprobado con ${nota} `;
+    }
+}
+
+
+
+function calcularDescuento() {
+
+    let monto = document.getElementById('monto').value;
+    let numeroConvertido = parseFloat(monto);
+
+    //monto *0.9
+    //parseInt
+    //parseFloat, decimales
+    if (numeroConvertido > 100) {
+        let descuento = numeroConvertido * 0.9;
+        document.getElementById('mensajeDescuento').innerText = `El monto con descuento es ${descuento}`;
+    } else {
+        document.getElementById('mensajeDescuento').innerText = `No tiene descuento, tienes que superar los $100`
+    }
+}
+
+function verificarMayorMenor() {
+    let edadSimple = document.getElementById('edadSimple').value;
+    let numeroConvertido = parseInt(edadSimple);
+
+    if (numeroConvertido >= 18) {
+        document.getElementById('mensajeEdadSimple').innerText = `La persona es mayor de edad`;
+    }else{
+        document.getElementById('mensajeEdadSimple').innerText = `La persona es menor de edad`;
+    }
 }
