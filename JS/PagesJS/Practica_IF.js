4278c72acf4dc25025c714df1114bcd2b17e8590
Isai-pexel <ibadillam@vlacademy.com>
2025-02-08 19:15:09 -0600

    Ejercicios de logica con if

--- JS/PagesJS/Practica_IF.js
+++ JS/PagesJS/Practica_IF.js
@@ -1,1 +1,12 @@
 console.log(`Funcionando Archivo: Practica_IF`);
+
+//paso 1
+
+function verificarEdad(){
+    let edad = document.getElementById('edad').value;
+    let pais = document.getElementById('pais').value;
+
+    console.log(edad)
+    console.log(pais)
+
+}
