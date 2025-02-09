e8146b0eeced232f8ea967fbff2514d806e4f805
Isai-pexel <ibadillam@vlacademy.com>
2025-02-09 00:52:06 -0600

    Ejercicio forEach

--- JS/PagesJS/Practica_CICLO.js
+++ JS/PagesJS/Practica_CICLO.js
@@ -7,3 +7,28 @@ let personas = [
     { nombre: "Pedro", edad: 40, activo: false }
 ];
 
+let clientes = [
+    { nombre: "Juan", edad: 25, activo: true },
+    { nombre: "Ana", edad: 17, activo: false },
+    { nombre: "Carlos", edad: 30, activo: true },
+    { nombre: "Laura", edad: 16, activo: true },
+    { nombre: "Pedro", edad: 40, activo: false }
+];
+
+console.log(personas[3])
+console.log(personas[1].nombre)
+//array [x]
+//objeto .nombre
+
+//bucles o ciclos
+//foreach
+//array quitelo y coloque su array
+//element serian las posiciones 0,1,2.....
+personas.forEach(element =>{
+    console.log(element)
+
+});
+
+clientes.forEach(element => {
+    console.log(element.nombre,element.activo)
+});
