1e12c3e0eea0e0d4cd46767a9f34a8a36eecad4e
Isai-pexel <ibadillam@vlacademy.com>
2025-02-10 20:33:08 -0600

    Consumir api y guardar los datos en una variable local

--- JS/PagesJS/API_Listado_Clientes.js
+++ JS/PagesJS/API_Listado_Clientes.js
@@ -3,32 +3,33 @@ console.log('Funcionando Archivo: API_Listado_Clientes');
 const Api_Usuarios = "https://jsonplaceholder.typicode.com/users"
 
 // Datos locales simulando la API
-let Datos_Locales = [];
+
 
 
 let datosFiltrados = []
 let contenedor = document.getElementById('tbody');
 
-datosFiltrados = Datos_Locales 
+
 
 //Aela Visualizar al ejecutar la pagina DOM
 document.addEventListener('DOMContentLoaded', () => {
     consultarDatos() 
 });
 
-
+//Consultar datros del api mediante fetch(ajax)
 function consultarDatos (){
 
     //fetchget
-    fetch(Api_Usuarios)
-        .then(response => response.json())
-        .then(result => {
+    fetch(Api_Usuarios) // 1-Se le indica el url de la api
+        .then(response => response.json())//2-Conversion de datos , json
+        .then(result => { //3-Resultados
             // custom error
-
+            datosFiltrados = result
             console.log(result)
         })
-        .catch(error => {
+        .catch(error => { //4- catch es en caso de error
             // common error
+            alert('Error')
             return null;
         });
 }
@@ -36,7 +37,7 @@ function consultarDatos (){
 
 //Consultar api clientes
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => { /
     consultarDatosConsola()
 });
 
